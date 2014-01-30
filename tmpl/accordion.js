9b622f5c38c26b2a043bad00e1464d6aac8e1f21
b2un0 <branko.wilhelm@gmail.com>
2014-01-30 21:09:57 +0100

    fix: js for accordion on ajax call

--- tmpl/accordion.js
+++ tmpl/accordion.js
@@ -1,12 +1,16 @@
-if (typeof jQuery != 'undefined') {
-    jQuery(document).ready(function ($) {
-        $('.mod_wow_blue_tracker h4').click(function () {
-            if ($(this).next('p').is(':visible')) {
-                $(this).next('p').slideUp('slow');
-            } else {
-                $('.mod_wow_blue_tracker p').slideUp('slow');
-                $(this).next('p').slideToggle('slow');
-            }
-        });
+window.wow = window.wow || {};
+
+window.wow.mod_wow_blue_tracker = function () {
+    jQuery('.mod_wow_blue_tracker h4').click(function () {
+        if (jQuery(this).next('p').is(':visible')) {
+            jQuery(this).next('p').slideUp('slow');
+        } else {
+            jQuery('.mod_wow_blue_tracker p').slideUp('slow');
+            jQuery(this).next('p').slideToggle('slow');
+        }
     });
+}
+
+if (typeof jQuery != 'undefined') {
+    jQuery(document).ready(window.wow.mod_wow_blue_tracker);
 }
