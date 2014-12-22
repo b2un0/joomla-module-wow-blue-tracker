110f1e9805c301a39bae0a8f945640d66c0139cd
b2un0 <branko.wilhelm@gmail.com>
2014-12-22 20:00:32 +0100

    new: wow lib usage new: media folder usage

--- helper.php
+++ helper.php
@@ -3,75 +3,25 @@
 /**
  * @author     Branko Wilhelm <branko.wilhelm@gmail.com>
  * @link       http://www.z-index.net
- * @copyright  (c) 2014 Branko Wilhelm
+ * @copyright  (c) 2014 - 2015 Branko Wilhelm
  * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 defined('_JEXEC') or die;
 
-abstract class ModWowBlueTrackerHelper
+class ModWowBlueTrackerHelper extends WoWModuleAbstract
 {
-    public static function getAjax()
+    protected function getInternalData()
     {
-        $module = JModuleHelper::getModule('mod_' . JFactory::getApplication()->input->get('module'));
-
-        if (empty($module)) {
-            return false;
-        }
-
-        JFactory::getLanguage()->load($module->module);
-
-        $params = new JRegistry($module->params);
-        $params->set('ajax', 0);
-
-        ob_start();
-
-        require(dirname(__FILE__) . '/' . $module->module . '.php');
-
-        return ob_get_clean();
-    }
-
-    public static function getData(JRegistry &$params)
-    {
-        if ($params->get('ajax')) {
-            return;
-        }
-
-        $url = 'http://blue.mmo-champion.com/rss/?language=' . $params->get('language', 'en-US');
-
-        $cache = JFactory::getCache('wow', 'output');
-        $cache->setCaching(1);
-        $cache->setLifeTime($params->get('cache_time', 60) * 60);
-
-        $key = md5($url);
-
-        if (!$result = $cache->get($key)) {
-            try {
-                $http = JHttpFactory::getHttp();
-                $http->setOption('userAgent', 'Joomla! ' . JVERSION . '; WoW Blue Tracker; php/' . phpversion());
-
-                $result = JHttpFactory::getHttp()->get($url, null, $params->get('timeout', 10));
-            } catch (Exception $e) {
-                return $e->getMessage();
-            }
-
-            $cache->store($result, $key);
-        }
-
-        if ($result->code != 200) {
-            return __CLASS__ . ' HTTP-Status ' . JHtml::_('link', 'http://wikipedia.org/wiki/List_of_HTTP_status_codes#' . $result->code, $result->code, array('target' => '_blank'));
-        }
-
-
-        $result->body = simplexml_load_string($result->body);
-
-        if (!($result->body instanceof SimpleXMLElement)) {
-            return JText::_('ERROR');
+        try {
+            $result = WoW::getInstance()->getAdapter('MMOChampion')->getData($this->params->module->get('language', 'en-US'));
+        } catch (Exception $e) {
+            return $e->getMessage();
         }
 
         $output = array();
         foreach ($result->body->channel->item as $item) {
-            if ($params->get('topics', 1)) {
+            if ($this->params->module->get('topics', 1)) {
                 if (strpos((string)$item->description, '[Blue Topic]') === 0) {
                     $item->description = str_replace('[Blue Topic]', '', (string)$item->description);
                     $output[] = $item;
@@ -80,7 +30,7 @@ abstract class ModWowBlueTrackerHelper
                 $output[] = $item;
             }
 
-            if (count($output) >= $params->get('rows', 10)) {
+            if (count($output) >= $this->params->module->get('rows', 10)) {
                 break;
             }
         }
--- tmpl/default.php
+++ tmpl/default.php
@@ -3,8 +3,12 @@
 /**
  * @author     Branko Wilhelm <branko.wilhelm@gmail.com>
  * @link       http://www.z-index.net
- * @copyright  (c) 2014 Branko Wilhelm
+ * @copyright  (c) 2014 - 2015 Branko Wilhelm
  * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @var        array $posts
+ * @var        stdClass $module
+ * @var        Joomla\Registry\Registry $params
  */
 
 defined('_JEXEC') or die;
@@ -13,8 +17,8 @@ if (version_compare(JVERSION, 3, '>=')) {
     JHtml::_('jquery.framework');
 }
 
-JFactory::getDocument()->addScript(JUri::base(true) . '/modules/' . $module->module . '/tmpl/default.js');
-JFactory::getDocument()->addStyleSheet(JUri::base(true) . '/modules/' . $module->module . '/tmpl/default.css');
+JFactory::getDocument()->addScript('media/' . $module->module . '/js/default.js');
+JFactory::getDocument()->addStyleSheet('media/' . $module->module . '/css/default.css');
 ?>
 <?php if ($params->get('ajax')) : ?>
     <div class="mod_wow_blue_tracker ajax"></div>
@@ -23,15 +27,15 @@ JFactory::getDocument()->addStyleSheet(JUri::base(true) . '/modules/' . $module-
         <?php foreach ($posts as $key => $post): ?>
             <li>
                 <span class="header">
-                    <?php echo JHtml::link($post->link, $post->title, array('target' => '_blank', 'title' => $post->title)); ?>
+                    <?php echo JHtml::_('link', $post->link, $post->title, array('target' => '_blank', 'title' => $post->title)); ?>
                 </span>
 
                 <div <?php echo ($key == 0 && $params->get('expand')) ? ' class="expand"' : ''; ?>>
-                <small><?php echo JHtml::date($post->pubDate, JText::_('DATE_FORMAT_LC2')); ?></small>
+                    <small><?php echo JHtml::_('date', $post->pubDate, JText::_('DATE_FORMAT_LC2')); ?></small>
 
                     <p>
                         <?php echo $post->description; ?>
-                        <?php echo JHtml::link($post->link, JText::_('MOD_WOW_BLUE_TRACKER_READMORE'), array('target' => '_blank', 'title' => $post->title)); ?>
+                        <?php echo JHtml::_('link', $post->link, JText::_('MOD_WOW_BLUE_TRACKER_READMORE'), array('target' => '_blank', 'title' => $post->title)); ?>
                     </p>
                 </div>
             </li>
